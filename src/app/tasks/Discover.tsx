431feeff55ab45a36a0c0ab75bba9b0871ffa121
Nhlanhla Msibi <nmsibi300@gmail.com>
2025-10-19 20:23:28 +0200

    Discover | GameFormat

--- src/app/tasks/Discover.tsx
+++ src/app/tasks/Discover.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
           <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-            <img
-              src="/car.jpg"
-              alt="Game 1"
-              className="w-full h-60 object-cover"
+            <Image 
+            src="/car.jpg"
+            alt="Game 1"
+            width={400}
+            height={240}
+            className="w-full h-60 object-cover"
             />
             <div className="p-4 flex justify-between">
               <p className="text-gray-100 font-semibold">Car Race 2</p>
@@ -13,10 +17,12 @@ export default function Page() {
             </div>
           </div>
           <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-            <img
-              src="/fps.jpg"
-              alt="Game 2"
-              className="w-full h-60 object-cover"
+            <Image 
+            src="/fps.jpg"
+            alt="Game 2"
+            width={400}
+            height={240}
+            className="w-full h-60 object-cover"
             />
             <div className="p-4 flex justify-between">
               <p className="text-gray-100 font-semibold">Shainline</p>
@@ -24,10 +30,12 @@ export default function Page() {
             </div>
           </div>
           <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-            <img
-              src="/fifa.jpg"
-              alt="Game 3"
-              className="w-full h-60 object-cover"
+            <Image 
+            src="/fifa.jpg"
+            alt="Game 3"
+            width={400}
+            height={240}
+            className="w-full h-60 object-cover"
             />
             <div className="p-4 flex justify-between">
               <p className="text-gray-100 font-semibold">Fifa 2019</p>
@@ -35,10 +43,12 @@ export default function Page() {
             </div>
           </div>
           <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-            <img
-              src="/gb.jpg"
-              alt="Game 4"
-              className="w-full h-60 object-cover"
+            <Image 
+            src="/gb.jpg"
+            alt="Game 4"
+            width={400}
+            height={240}
+            className="w-full h-60 object-cover"
             />
             <div className="p-4 flex justify-between">
               <p className="text-gray-100 font-semibold">Gunship Battle</p>
