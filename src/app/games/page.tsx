5d14fb1f0fe20ddd05bc7dc7f787b267ba15774b
Nhlanhla Msibi <nmsibi300@gmail.com>
2025-10-21 05:04:03 +0200

    Games Page Update

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -4,81 +4,82 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div>
-        <Sidebar />
+      <Sidebar />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-          <Image
-            src="/car.jpg"
-            alt="Game 1"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/car.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Red Carer 2
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">    
-          <Image
-            src="/cr.jpg"
-            alt="Game 2"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/cr.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Clash Royale
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-          <Image
-            src="/fn.jpg"
-            alt="Game 3"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/fn.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Forth Night
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-          <Image
-            src="/urt2.jpg"
-            alt="Game 4"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/urt2.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Euro Truck 2
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-          <Image
-            src="/fps.jpg"
-            alt="Game 5"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/FPS.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Critical Striker
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-         <Image
-            src="/cod.jpg"
-            alt="Game 6"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/cod.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Call Of Duty
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-         <Image
-            src="/gb.jpg"
-            alt="Game 7"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/gb.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Gunship Battle
+          </h2>
         </div>
-        <div className="bg-black-sec text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer">
-         <Image
-            src="/fifa.jpg"
-            alt="Game 8"
-            width={700}
-            height={400}
-            className="w-full h-80 object-cover"
-          />
+        <div className="bg-black-sec bg-cover bg-center text-white rounded-xl shadow-md overflow-hidden hover:shadow-2xl hover:scale-[1.03] hover:brightness-130 transition-all duration-300 transform cursor-pointer flex items-end justify-center h-80"
+          style={{
+            backgroundImage: "url('/fifa.jpg')",
+          }}
+        >
+          <h2 className="text-2xl font-bold mb-4 text-white text-shadow-2xs text-shadow-cyan-950">
+            Fifa 2025
+          </h2>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
